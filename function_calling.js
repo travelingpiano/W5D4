5184941cb779056d83e941204049d926ee0b4401
Li Hsuan Lu <lihsuanlu@gmail.com>
2017-06-29 17:29:57 -0700

    mybind

---
+++ function_calling.js
@@ -0,0 +1,25 @@
+Function.prototype.myBind = function (context) {
+  return (...array)=>{this.apply(context,array)};
+}
+
+class Lamp {
+  constructor() {
+    this.name = "a lamp";
+  }
+}
+
+const turnOn = function(kind) {
+   console.log("Turning on the " + kind + " " + this.name);
+}
+
+name = "Bob";
+
+const lamp = new Lamp();
+
+turnOn(); // should not work the way we want it to
+
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
+
+boundTurnOn("large"); // should say "Turning on a lamp"
+myBoundTurnOn("large"); // should say "Turning on a lamp"
